f4dfaa0cd0c80c7f61e5463c5ce42a06b0fd5bde
josera980 <josera980@hotmail.com>
2021-12-09 11:32:58 -0400

    Adding links with react-routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 import UsersList from "./components/Lists/UsersList";
 import UserInputs from "./components/Inputs/UserInputs";
@@ -30,11 +31,36 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <h1>JR Exercise 1 -- KLK</h1>
-      <UserInputs onAddUser={addUserHandler} />
-      {content}
-    </div>
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/form">Form</Link>
+          </li>
+          <li>
+            <Link to="table">Table</Link>
+          </li>
+        </ul>
+        <Routes>
+          <Route exact path="/" element={<h1>JR Exercise 1 -- KLK</h1>}></Route>
+          <Route
+            exact
+            path="/form"
+            element={<UserInputs onAddUser={addUserHandler} />}
+          ></Route>
+          <Route exact path="/table" element={content}></Route>
+        </Routes>
+      </div>
+    </Router>
+
+    // <div className="App">
+    //   <h1>JR Exercise 1 -- KLK</h1>
+    //   <UserInputs onAddUser={addUserHandler} />
+    //   {content}
+    // </div>
   );
 }
 
